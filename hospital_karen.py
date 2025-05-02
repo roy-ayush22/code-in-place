fa0d81b835dc1515e38316fdae0378847f7f898b
Ayush Roy <ayush30085@gmail.com>
2025-05-03 00:58:35 +0530

    initial commit

---
+++ hospital_karen.py
@@ -0,0 +1,69 @@
+from karel.stanfordkarel import *
+
+# Here is a place to program your Section problem
+
+
+
+def main():
+    move()
+    move()
+    turn_left()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    turn_right()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_left()
+    move_block()
+    move_beeper()
+    for i in range(4):
+        move()
+    turn_left()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    turn_right()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_left()
+
+
+
+
+
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def move_block():
+    for i in range(5):
+        move()
+
+def move_beeper():
+    turn_left()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_right()
+    move()
+    put_beeper()
+    turn_right()
+    for i in range(2):
+        move()
+        put_beeper()
+    turn_left()
+
+if __name__ == '__main__':
+    main()
