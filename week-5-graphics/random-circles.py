86d8cab5c879b691a1074ca0c6e039214f897bb3
Ayush Roy <ayush30085@gmail.com>
2025-05-24 00:32:34 +0530

    week-5

---
+++ week-5-graphics/random-circles.py
@@ -0,0 +1,36 @@
+from graphics import Canvas
+import random
+
+CANVAS_WIDTH = 300
+CANVAS_HEIGHT = 300
+CIRCLE_SIZE = 20
+N_CIRCLES = 20
+
+def main():
+    print('Random Circles')
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+
+    for i in range(N_CIRCLES):
+        make_circle(canvas)
+    
+def make_circle(canvas):
+    x1 = random.randint(0, CANVAS_WIDTH)
+    x2 = x1 + CIRCLE_SIZE
+    y1 = random.randint(0, CANVAS_HEIGHT)
+    y2 = y1 + CIRCLE_SIZE
+
+    oval = canvas.create_oval(x1,y1, x2, y2, random_color() )
+    
+   
+    
+def random_color():
+    """
+    This is a function to use to get a random color for each circle. We have
+    defined this for you and there is no need to edit code in this function,
+    but feel free to read it over if you are interested. 
+    """
+    colors = ['blue', 'purple', 'salmon', 'lightblue', 'cyan', 'forestgreen']
+    return random.choice(colors)
+
+if __name__ == '__main__':
+    main()
